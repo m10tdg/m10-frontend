50187d52d5eca5556bf2a475c0453016f035988c
Abdoul Faride Bassirou Alzouma <abdoulfaridbassirou7898@gmail.com>
2026-04-03 10:36:16 +0300

    feat: setting up the routing logic

---
+++ src/app/router.tsx
@@ -0,0 +1,7 @@
+// src/app/router.tsx
+import { createBrowserRouter } from "react-router-dom";
+import Home from "@/pages/home";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+]);
