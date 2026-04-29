8c39cb007385dabbd5e20b1eb7dc6c2f706e1151
Abdoul Faride Bassirou Alzouma <abdoulfaridbassirou7898@gmail.com>
2026-04-29 16:21:37 +0300

    feat: Adding the system setting page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -8,6 +8,7 @@ import { ProfilePage } from "@/pages/profile/Profile";
 import { UserManagement } from "@/pages/adminUserManagement/UserManagement";
 import { LicenseManagement } from "@/pages/license/License";
 import { LogsReports } from "@/pages/logsReports/LogsReports";
+import { SystemSettings } from "@/pages/systemSettings.tsx/systemSettings";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -18,4 +19,5 @@ export const router = createBrowserRouter([
   {path: "/admin/users", element: < UserManagement /> },
   {path: "/admin/licenses", element: <LicenseManagement /> },
   {path: "/admin/logs-reports", element: <LogsReports /> },
+  {path:"/admin/settings", element: <SystemSettings /> },
 ]);
